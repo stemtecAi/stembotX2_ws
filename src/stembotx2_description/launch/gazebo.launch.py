35b69f6e13b2df71f96fd5a837a14ceb5a47ed4e
Kou-shik2004 <koushik20040804@gmail.com>
2024-07-22 23:12:51 +0530

    added new launch files

--- src/stembotx2_description/launch/gazebo.launch.py
+++ src/stembotx2_description/launch/gazebo.launch.py
@@ -1,60 +1,73 @@
-import os
-from launch import LaunchDescription
-from launch.actions import IncludeLaunchDescription, RegisterEventHandler
-from launch.launch_description_sources import PythonLaunchDescriptionSource
-from launch.event_handlers import OnProcessExit
 from launch_ros.actions import Node
-from ament_index_python.packages import get_package_share_directory
+from launch_ros.substitutions import FindPackageShare
+from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import PathJoinSubstitution
+import os
 import xacro
+from ament_index_python.packages import get_package_share_directory
+
 
 def generate_launch_description():
-    package_name = 'stembotx2_description'
-    pkg_share = get_package_share_directory(package_name)
+    share_dir = get_package_share_directory('stembotx2_description')
 
-    # Process URDF file
-    xacro_file = os.path.join(pkg_share, 'urdf', '2wd.urdf.xacro')
+    xacro_file = os.path.join(share_dir, 'urdf', 'ag.urdf.xacro')
     robot_description_config = xacro.process_file(xacro_file)
-    robot_description = {'robot_description': robot_description_config.toxml()}
+    robot_urdf = robot_description_config.toxml()
 
-    #world = os.path.join(pkg_share,'worlds','fieldv2.sdf')
-
-
-    # Gazebo launch
-    gazebo = IncludeLaunchDescription(
-        PythonLaunchDescriptionSource([os.path.join(
-            get_package_share_directory('gazebo_ros'), 'launch', 'gazebo.launch.py')]),
-    )
-
-    # Spawn the robot in Gazebo
-    spawn_entity = Node(
-        package='gazebo_ros', 
-        executable='spawn_entity.py',
-        arguments=['-topic', 'robot_description',
-                   '-entity', 'agv',
-                   ],
-        output='screen'
-    )
-
-    # Start Robot State Publisher
-    robot_state_publisher = Node(
+    robot_state_publisher_node = Node(
         package='robot_state_publisher',
         executable='robot_state_publisher',
-        output='screen',
-        parameters=[robot_description]
+        name='robot_state_publisher',
+        parameters=[
+            {'robot_description': robot_urdf}
+        ]
     )
 
-    # Launch RViz
-    rviz = Node(
-        package='rviz2',
-        executable='rviz2',
-        name='rviz2',
-        arguments=['-d', os.path.join(pkg_share, 'rviz', 'view_urdf.rviz')],
+    joint_state_publisher_node = Node(
+        package='joint_state_publisher',
+        executable='joint_state_publisher',
+        name='joint_state_publisher'
+    )
+
+    gazebo_server = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource([
+            PathJoinSubstitution([
+                FindPackageShare('gazebo_ros'),
+                'launch',
+                'gzserver.launch.py'
+            ])
+        ]),
+        launch_arguments={
+            'pause': 'true'
+        }.items()
+    )
+
+    gazebo_client = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource([
+            PathJoinSubstitution([
+                FindPackageShare('gazebo_ros'),
+                'launch',
+                'gzclient.launch.py'
+            ])
+        ])
+    )
+
+    urdf_spawn_node = Node(
+        package='gazebo_ros',
+        executable='spawn_entity.py',
+        arguments=[
+            '-entity', 'Test',
+            '-topic', 'robot_description'
+        ],
         output='screen'
     )
 
     return LaunchDescription([
-        gazebo,
-        spawn_entity,
-        robot_state_publisher,
-        rviz
+        robot_state_publisher_node,
+        joint_state_publisher_node,
+        gazebo_server,
+        gazebo_client,
+        urdf_spawn_node,
     ])
